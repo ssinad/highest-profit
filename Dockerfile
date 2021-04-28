7a2eb03051fa1dc82b6dc20ec9a39973784c5c5b
ssinad <27953063+ssinad@users.noreply.github.com>
2021-04-28 14:58:23 -0600

    Change input to url

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM python:3.8.9-slim-buster
 
+WORKDIR /scripts
+
+COPY highest-profit.py requirements.txt /scripts/
+
+RUN pip install -r requirements.txt && ln -s /scripts/highest-profit.py /usr/local/bin
+
 WORKDIR /root
 
-COPY highest-profit.py data.csv requirements.txt ./
-
-RUN pip install -r requirements.txt
-
-ENTRYPOINT [ "./highest-profit.py" ]
+ENTRYPOINT [ "highest-profit.py" ]
--- highest-profit.py
+++ highest-profit.py
@@ -6,7 +6,9 @@ import numpy as np
 
 ## Part 1
 
-data = pd.read_csv('data.csv', na_values='N.A.')
+data_url = "https://gist.github.com/bobbae/b4eec5b5cb0263e7e3e63a6806d045f2/raw/279b794a834a62dc108fc843a72c94c49361b501/data.csv"
+
+data = pd.read_csv(data_url, na_values='N.A.')
 print(f'There are {data.shape[0]} rows including non-numeric values')
 data = data.dropna()
 data.iloc[:, -1] = data.iloc[:, -1].astype(np.float64)
